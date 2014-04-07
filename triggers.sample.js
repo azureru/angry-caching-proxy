13b74fa9c47fc4492ec6cb4e77c161c1fecf2091
azureru <eru.saputra@gmail.com>
2014-04-07 18:19:44 +0700

    Adding tmdb and trak to proxy

--- triggers.sample.js
+++ triggers.sample.js
@@ -2,6 +2,8 @@ module.exports = {
     "mata2sites": function isblitzAndCinemaDevelopment(req, res) {
     	 if (/blitzmegaplex.com$/.test(req.headers.host)) return true;
     	 if (/21cineplex.com$/.test(req.headers.host)) return true;
+         if (/api.themoviedb.org/.test(req.headers.host)) return true;
+         if (/api.trakt.tv/.test(req.headers.host)) return true;
     },
 
     "ismayaApi": function isIsmayaApi(req, res) {
