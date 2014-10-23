c1bc1ec6a956c4da4f5dd4d28e564873d010b4ee
azureru <eru.saputra@gmail.com>
2014-10-23 19:58:16 +0700

    Add another site to prox

--- triggers.sample.js
+++ triggers.sample.js
@@ -1,12 +1,14 @@
 module.exports = {
     "mata2sites": function isblitzAndCinemaDevelopment(req, res) {
-    	 if (/blitzmegaplex.com$/.test(req.headers.host)) return true;
-    	 if (/21cineplex.com$/.test(req.headers.host)) return true;
+         if (/blitzmegaplex.com$/.test(req.headers.host)) return true;
+         if (/21cineplex.com$/.test(req.headers.host)) return true;
          if (/api.themoviedb.org/.test(req.headers.host)) return true;
          if (/api.trakt.tv/.test(req.headers.host)) return true;
+         if (/cinemaxxtheater.com/.test(req.headers.host)) return true;
+         if (/youtube.com/.test(req.headers.host)) return true;
     },
 
     "ismayaApi": function isIsmayaApi(req, res) {
-    	 if (/ismayagroup.com$/.test(req.headers.host)) return true;
+         if (/ismayagroup.com$/.test(req.headers.host)) return true;
     }
 };
