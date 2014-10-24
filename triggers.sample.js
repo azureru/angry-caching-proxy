49ee35f520d43749c805c8d4e5c6a72ee156a2d2
azureru <eru.saputra@gmail.com>
2014-10-24 19:21:26 +0700

    Change the triggers

--- triggers.sample.js
+++ triggers.sample.js
@@ -1,14 +1,21 @@
 module.exports = {
-    "mata2sites": function isblitzAndCinemaDevelopment(req, res) {
+    "blizt": function blitz(req, res) {
          if (/blitzmegaplex.com$/.test(req.headers.host)) return true;
+     },
+    "21cineplex" : function cineplex(req, res) {
          if (/21cineplex.com$/.test(req.headers.host)) return true;
+    },
+    "cinemaxx" : function cinemaxx(req, res) {
+         if (/cinemaxxtheater.com/.test(req.headers.host)) return true;
+    },
+    "traktmdb" : function traktmdb(req, res) {
          if (/api.themoviedb.org/.test(req.headers.host)) return true;
          if (/api.trakt.tv/.test(req.headers.host)) return true;
-         if (/cinemaxxtheater.com/.test(req.headers.host)) return true;
-         if (/youtube.com/.test(req.headers.host)) return true;
     },
-
     "ismayaApi": function isIsmayaApi(req, res) {
          if (/ismayagroup.com$/.test(req.headers.host)) return true;
+    },
+    "optimizer" : function optimizer(req, res) {
+        return false;
     }
 };
